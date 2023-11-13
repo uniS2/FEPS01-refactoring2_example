7a1a9a4f399150512ed46d1dc43e65671317f5c7
uniS2 <s2oie1010@gmail.com>
2023-11-13 15:58:01 +0900

    Class: 변수 인라인 (빼내기)

--- ch6/6-4.js
+++ ch6/6-4.js
@@ -1,6 +1,7 @@
+//# 변수 인라인 (빼내기)
 export function isDeliveryFree(anOrder) {
-  let basePrice = anOrder.basePrice;
-  return basePrice > 1000;
+  // let basePrice = anOrder.basePrice;
+  return anOrder.basePrice > 1000;
 }
 
 // 변수 인라인하기 (책 6.4)
